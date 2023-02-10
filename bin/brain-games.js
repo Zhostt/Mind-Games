d45c1ea00eaf00c408043b4ba2309c32e8da2624
Zhostt <crazyplumber@yandex.ru>
2023-02-10 15:35:10 +0300

    updated gitignore - installed vscode for wsl

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,5 +1,6 @@
-#!/usr/bin/env node
-// import readlineSync from 'readline-sync';
+/* #!/usr/bin/env node    // Это шабанг, он добавляет к исполняюще строчке то что в нем прописано.
+Но в моем VS Code он не работает, а в REPL работает все равно, потому отключен. */
+
 import greeting from '../src/cli.js';
 
 console.log('node bin/brain-games.js\nWelcome to the Brain Games!');
