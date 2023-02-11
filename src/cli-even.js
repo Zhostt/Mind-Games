c4764fbfc5815f7a510c7efe83539c627df40ff4
Zhostt <crazyplumber@yandex.ru>
2023-02-11 14:52:15 +0300

    Fixed linter errrors (mostly expected linebreaks to be LF)

--- src/cli-even.js
+++ src/cli-even.js
@@ -1,66 +1,44 @@
-/* eslint-disable linebreak-style */
-/* eslint-disable max-len */
-// functions for game "even or odd", imported in brain-even.js;
-
-import readlineSync from 'readline-sync';
+import * as commonFunc from './index.js';
 
 let name = '';
 let number = 0;
-let evenity = 'no';
-let userAnswer = '';
+let correctAnswer = '';
 let score = 0;
+let userAnswer = '';
 
-export const greeting = () => {
-  console.log('brain-games\nWelcome to the Brain Games!');
-  name = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${name}!`);
+const taskEven = () => {
   console.log('Answer "yes" if the number is even, otherwise answer "no".');
 };
 
-const getRandomInt = (min, max) => {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min) + min); // The maximum is exclusive and the minimum is inclusive
-};
-
-export const createNumberSetEvenity = () => {
-  number = getRandomInt(1, 100);
-  console.log(`Question: ${number}`);
+const createNumberSetEvenity = () => {
+  number = commonFunc.getRandomInt(1, 100);
   if (number % 2 === 0) {
-    evenity = 'yes';
+    correctAnswer = 'yes';
   } else {
-    evenity = 'no';
+    correctAnswer = 'no';
   }
 };
 
-export const answerGetEvaluate = () => {
-  userAnswer = readlineSync.question('Your answer: ');
-  if (userAnswer === evenity) {
-    console.log('Correct!');
-    return true;
-  }
-  // eslint-disable-next-line no-else-return
-  else {
-    console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${evenity}'.
-    Let's try again, ${name}!`);
-    return false;
-  }
+const questionEven = () => {
+  console.log(`Question: ${number}`);
 };
 
 const startEvenGame = () => {
-  greeting();
+  name = commonFunc.greeting();
+  taskEven();
   for (let round = 1; round <= 3; round += 1) {
-    getRandomInt();
     createNumberSetEvenity();
-    // answerGetEvaluate();
-    if (answerGetEvaluate() === false) {
+    questionEven();
+    userAnswer = commonFunc.getAnswerStr();
+    if (commonFunc.evaluateAnswer(userAnswer, correctAnswer) === false) {
+      commonFunc.failMessageLastPart(userAnswer, correctAnswer, name);
       break;
     } else {
       score += 1;
     }
   }
   if (score === 3) {
-    console.log(`Congratulations, ${name}!`);
+    commonFunc.victoryMessage(name);
   }
 };
 
