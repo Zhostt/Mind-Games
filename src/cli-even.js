d3e4bc7e4386fb78810036b1f873f7ee40ca52bc
Zhostt <crazyplumber@yandex.ru>
2023-03-15 18:42:52 +0300

    Revert "deleted cli.js, cli-even.js as obsolete"
    This reverts commit 8c35759fc57fa789f5bebf73a3ea4204cc6b1b40.

---
+++ src/cli-even.js
@@ -0,0 +1,45 @@
+import * as commonFunc from './index.js';
+
+let name = '';
+let number = 0;
+let correctAnswer = '';
+let score = 0;
+let userAnswer = '';
+
+const taskEven = () => {
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+};
+
+const createNumberSetEvenity = () => {
+  number = commonFunc.getRandomInt(1, 100);
+  if (number % 2 === 0) {
+    correctAnswer = 'yes';
+  } else {
+    correctAnswer = 'no';
+  }
+};
+
+const questionEven = () => {
+  console.log(`Question: ${number}`);
+};
+
+const startEvenGame = () => {
+  name = commonFunc.greeting();
+  taskEven();
+  for (let round = 1; round <= 3; round += 1) {
+    createNumberSetEvenity();
+    questionEven();
+    userAnswer = commonFunc.getAnswerStr();
+    if (commonFunc.evaluateAnswer(userAnswer, correctAnswer) === false) {
+      commonFunc.failMessageLastPart(userAnswer, correctAnswer, name);
+      break;
+    } else {
+      score += 1;
+    }
+  }
+  if (score === 3) {
+    commonFunc.victoryMessage(name);
+  }
+};
+
+export default startEvenGame;
